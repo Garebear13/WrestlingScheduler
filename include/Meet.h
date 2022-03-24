e91757ecf10d7afd87489c3f39ca3ff27ca5df50
Garebear13 <gdion1966@gmail.com>
2022-03-24 11:41:35 -0400

    Write Meet Class

---
+++ include/Meet.h
@@ -0,0 +1,20 @@
+#ifndef _MEET_H
+#define _MEET_H
+
+#include "Team.h"
+
+class meet {
+
+    public:
+
+        meet(team team1, team team2);
+        meet(team team1, team team2, team team3);
+
+        team getTeam1() const;
+        team getTeam2() const;
+
+    private:
+        team team1;
+        team team2;
+
+}
